bd3d0157489f11d77e5dafcb684a186f5e38c7d7
bhadraaa <bhadra.j05@gmail.com>
2026-04-12 22:59:45 +0530

    router issue checking

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,17 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
-});
-
-api.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
 });
 
 export default api;
